67984e70fe6c467beac9f9eb69221193260af229
millen1m <maxim.millen@gmail.com>
2019-11-07 15:30:10 +1300

    replaced con.py with cc.py

--- o3seespy/__init__.py
+++ o3seespy/__init__.py
@@ -1,7 +1,7 @@
 from o3seespy.opensees_instance import OpenseesInstance
 from o3seespy import exceptions
-from o3seespy import con
-from o3seespy import con as static  # deprecated
+from o3seespy import cc
+from o3seespy import cc as static  # deprecated
 from o3seespy.command import node, algorithm, rayleigh, test_check, uniaxial_material, element, nd_material
 from o3seespy.command.common import *
 from o3seespy.command import section, beam_integration, transformation, constraint, numberer, system
