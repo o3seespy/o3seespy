3c5679a46fc8a6936d937be54a6dd01454c6564e
millen1m <maxim.millen@gmail.com>
2019-11-28 13:23:28 +1300

    Added all analysis options.

--- o3seespy/command/analysis.py
+++ o3seespy/command/analysis.py
@@ -5,10 +5,41 @@ class AnalysisBase(OpenseesObject):
     op_base_type = "analysis"
 
 
+class Static(AnalysisBase):
+    op_type = "Static"
+
+    def __init__(self, osi):
+
+        self._parameters = [self.op_type]
+        self.to_process(osi)
+
+
 class Transient(AnalysisBase):
     op_type = "Transient"
 
     def __init__(self, osi):
 
         self._parameters = [self.op_type]
+        self.to_process(osi)
+
+
+class VariableTransient(AnalysisBase):
+    op_type = "VariableTransient"
+
+    def __init__(self, osi):
+
+        self._parameters = [self.op_type]
+        self.to_process(osi)
+
+
+class PFEM(AnalysisBase):
+    op_type = "PFEM"
+
+    def __init__(self, osi, dt_max, dt_min, gravity, ratio=0.5):
+        self.dt_max = dt_max
+        self.dt_min = dt_min
+        self.gravity = gravity
+        self.ratio = ratio
+
+        self._parameters = [self.op_type, self.dt_max, self.dt_min, self.gravity, self.ratio]
         self.to_process(osi)
