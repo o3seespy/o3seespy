0ddbf31712146e405af70d025c68589a13076dc0
millen1m <maxim.millen@gmail.com>
2020-01-27 15:46:45 +1300

    increment to trigger build.

--- o3seespy/__about__.py
+++ o3seespy/__about__.py
@@ -1,4 +1,4 @@
 __project__ = "o3seespy"
 __author__ = "Maxim Millen & Minjie Zhu"
-__version__ = "3.1.0.1"
+__version__ = "3.1.0.2"
 __license__ = "MIT"
---
+++ trigger_deploy.py
@@ -0,0 +1,18 @@
+import subprocess
+import py
+
+
+about = {}
+with open("o3seespy/__about__.py") as fp:
+    exec(fp.read(), about)
+
+
+version = about['__version__']
+
+failures = py.test.cmdline.main()
+if failures == 0:
+    subprocess.check_call(["git", "tag", version, "-m", "version %s" % version])
+    subprocess.check_call(["git", "push", "--tags"])
+
+# git push --tags origin pypi
+# git tag 0.5.2 -m "version 0.5.2"
